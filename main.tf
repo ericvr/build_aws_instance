972dc4cee11d75188cfb036cde68086ef30aa254
Erick <eeric466@gmail.com>
2022-08-18 12:57:00 -0500

    s

--- main.tf
+++ main.tf
@@ -13,12 +13,18 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
+
+resource "aws_instance" "lab_server_01" {
   ami           = "ami-052efd3df9dad4825"
   instance_type = "t2.micro"
-
+  security_groups = ["launch-wizard-5","default"]
+  key_name= "key_serv_pruebas"
   tags = {
-    Name = "BuildInstanceV1"
+    Name = "Server_terraform_lab01"
   }
 }
-//dfdfiuhg
+
+output "instance_public_ip" {
+  description = "Obtener la IP publica de mi instancia"
+  value = aws_instance.lab_server_01.public_ip  // aws_instance.$NOMBRE-RECURSO-TIPO-aws_instance$.public_ip
+}
