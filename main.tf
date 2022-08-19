dbf5a4ca803617b8eca787cf1254fd96abe19a9d
ericvr <eeric466@gmail.com>
2022-08-18 19:55:32 -0500

    t

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-variable "reponame" {}
-variable "container_port" {}
+//variable "reponame" {}
+//variable "container_port" {}
 
 provider "aws" {
   region  = "us-east-1"
@@ -19,10 +19,10 @@ provider "aws" {
 
 resource "aws_instance" "lab_server_01" {
   image = docker_image.nginx.latest
-  name  = var.reponame   // Cambiarla dinámicamente por la Variable env.DOCKER_REPO que está en el Jenkins
+  name  = eeric466      //var.reponame    Cambiarla dinámicamente por la Variable env.DOCKER_REPO que está en el Jenkins
   ports {
     internal = 80
-    external = var.container_port  // Cambiarla dinámicamente por la variable CONTAINER_PORT que está en el Jenkins.
+    external = 82       //var.container_port  // Cambiarla dinámicamente por la variable CONTAINER_PORT que está en el Jenkins.
   }
   ami           = "ami-052efd3df9dad4825"
   instance_type = "t2.micro"
