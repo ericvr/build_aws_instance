054f08620d6f90fb0ce07a70b8abeb62c3ef4ec9
ericvr <eeric466@gmail.com>
2022-08-18 22:02:11 -0500

    h

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_instance" "lab_server_01" {
   security_groups = ["launch-wizard-1","default"]
   key_name= "key_serv_pruebas"
   tags = {
-    Name = "serv_tf_lab13"
+    Name = "serv_tf_lab14"
   }
 }
 
